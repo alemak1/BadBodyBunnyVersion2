ff30930ee64f3b40946a552b5fa60d70d2b63070
yongrenwang <czie.alexmak@gmail.com>
2017-04-28 12:12:16 +0800

    Added the UFO scene

--- GameViewController.swift
+++ GameViewController.swift
@@ -28,9 +28,9 @@ class GameViewController: UIViewController {
         let baseScene = BaseScene(size: view.bounds.size)
         let platformerBaseScene = PlatformerBaseScene(sksFileName: "IslandScene", size: view.bounds.size)
         let beeScene = LevelBScene(sksFileName: "BeeScene", size: view.bounds.size)
-        let treeScene = LevelTScene(sksFileName: "ForestScene", size: view.bounds.size)
-        
-        skView.presentScene(treeScene)
+        let treeScene = LevelTScene(sksFileName: "AlienScene", size: view.bounds.size)
+        let ufoScene = LevelUScene(sksFileName: "AlienScene", size: view.bounds.size)
+        skView.presentScene(ufoScene)
         
         
     }
--- LevelUScene.swift
+++ LevelUScene.swift
@@ -1,23 +1,25 @@
 //
-//  LevelTScene.swift
+//  LevelUScene.swift
 //  Badboy Bunny Learns the ABCs
 //
-//  Created by Aleksander Makedonski on 4/27/17.
+//  Created by Aleksander Makedonski on 4/28/17.
 //  Copyright © 2017 AlexMakedonski. All rights reserved.
 //
 
 import Foundation
+
 import GameplayKit
 import SpriteKit
 
-class LevelTScene: PlatformerBaseScene{
+class LevelUScene: PlatformerBaseScene{
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
+        self.physicsWorld.gravity = CGVector(dx: 0.00, dy: -3.00)
         
         if let playerNode = player.component(ofType: RenderComponent.self)?.node{
-           // playerNode.position = CGPoint(x: -336, y: 646)
-           // playerNode.position = CGPoint(x: 3700, y: 80)
+            // playerNode.position = CGPoint(x: -336, y: 646)
+            // playerNode.position = CGPoint(x: 3700, y: 80)
         }
     }
     
